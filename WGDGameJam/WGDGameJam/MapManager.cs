2737f02f3ed32b6604382eab60861841abe52abe
kbarrett <x_kimmi_13_x@hotmail.co.uk>
2012-02-24 22:22:27 +0000

    map stuff

---
+++ WGDGameJam/WGDGameJam/MapManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace WGDGameJam
+{
+    class MapManager
+    {
+        Square[,] map;
+        Texture2D grassTexture;
+        Texture2D hedgeTexture;
+        Texture2D foodTexture;
+        CowPiece headPiece;
+        int size;
+
+        public MapManager(int size, Texture2D grassTexture, Texture2D hedgeTexture, Texture2D foodTexture, CowPiece head)
+        {
+            this.size = size;
+            map = new Square[size,size];
+            this.grassTexture = grassTexture;
+            this.hedgeTexture = hedgeTexture;
+            this.foodTexture = foodTexture;
+            headPiece = head;
+            createMap();
+        }
+
+        public void createMap()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (i == 4) { map[i, j] = new Square(i, j, grassTexture, Color.LightGreen, false, false); }
+                    else { map[i, j] = new Square(i, j, grassTexture, Color.Green, false, false); }
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            for(int i=0; i<size; i++)
+            {
+                for(int j=0; j<size; j++)
+                {
+                    map[i, j].Draw(spriteBatch, headPiece);
+                }
+            }
+        }
+    }
+}
---
+++ WGDGameJam/WGDGameJam/Square.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace WGDGameJam
+{
+    class Square
+    {
+        Texture2D texture;
+        Color colour;
+        private bool blocking;
+        private bool food;
+        int xlocation;
+        int ylocation;
+
+        public Square(int x, int y, Texture2D texture, Color colour, bool blocking, bool food)
+        {
+            this.texture = texture;
+            this.colour = colour;
+            this.blocking = blocking;
+            this.food = food;
+            xlocation = x;
+            ylocation = y;
+        }
+
+        public bool isBlocking()
+        {
+            return blocking;
+        }
+        public bool containsFood()
+        {
+            return food;
+        }
+        public void Draw(SpriteBatch spriteBatch, CowPiece head)
+        {
+            Vector2 position = new Vector2(- head.headPosition.X + xlocation * 50, - head.headPosition.Y + ylocation * 50);
+            spriteBatch.Draw(texture, position, colour);
+        }
+    }
+}
