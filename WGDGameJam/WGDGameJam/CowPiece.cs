b7a3673070dd60fe9bce920954f598a5ea67a7ef
thk123 <thekileyenator@gmail.com>
2012-02-24 20:02:16 +0000

    Implemented moving in a generic cow piece

--- WGDGameJam/WGDGameJam/CowPiece.cs
+++ WGDGameJam/WGDGameJam/CowPiece.cs
@@ -10,7 +10,11 @@ namespace WGDGameJam
     class CowPiece
     {
         const Point startPoint = new Point(50, 50);
+        const Vector2 squareSize = new Vector2(50, 50);
+        const Vector2 middle = new Vector2(375, 275);
         protected Texture2D texture;
+        protected CowPiece nextPiece;
+        protected DirectionToMove lastMoveDirection;
 
         Point location;
         Point headPosition;
@@ -19,26 +23,60 @@ namespace WGDGameJam
         {
             this.texture = texture;
             headPosition = startPoint;
+            nextPiece = null;
         }
-
-
 
         public void Update(GameTime gameTime, DirectionToMove moveDirection, Point newHeadPosition)
         {
-            headPosition = newHeadPosition;
             switch (moveDirection)
             {
                 case DirectionToMove.down:
                     {
-
+                        location.Y -= 1;
+                        break;
                     }
 
+                case DirectionToMove.up:
+                    {
+                        location.Y += 1;
+                        break;
+                    }
+
+                case DirectionToMove.left:
+                    {
+                        location.X -= 1;
+                        break;
+                    }
+
+                case DirectionToMove.right:
+                    {
+                        location.X += 1;
+                        break;
+                    }
             }
+
+            if (newHeadPosition.X == -1)
+            {
+                headPosition = location;
+            }
+            else
+            {
+                headPosition = newHeadPosition;
+            }
+
+            if (nextPiece != null)
+            {
+                nextPiece.Update(gameTime, lastMoveDirection, headPosition);
+            }
+
+            lastMoveDirection = moveDirection;
         }
 
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            //spriteBatch.Draw(texture, 
+            Point offsetFromHead = new Point(location.X - headPosition.X, location.Y - headPosition.Y);
+            Vector2 position = middle + new Vector2(squareSize.X * offsetFromHead.X, squareSize.Y * offsetFromHead.Y);
+            spriteBatch.Draw(texture, position, Color.White);
         }
     }
 
