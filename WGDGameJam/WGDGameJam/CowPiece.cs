e97c23db8ad53893a17c57a8c768d15d1e679a4b
thk123 <thekileyenator@gmail.com>
2012-02-24 19:25:08 +0000

    Added cow peice

---
+++ WGDGameJam/WGDGameJam/CowPiece.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace WGDGameJam
+{
+    class CowPiece
+    {
+        const Point startPoint = new Point(50, 50);
+        protected Texture2D texture;
+
+        Point location;
+        Point headPosition;
+
+        public CowPiece(Texture2D texture)
+        {
+            this.texture = texture;
+            headPosition = startPoint;
+        }
+
+
+
+        public void Update(GameTime gameTime, DirectionToMove moveDirection, Point newHeadPosition)
+        {
+            headPosition = newHeadPosition;
+            
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            //spriteBatch.Draw(texture, 
+        }
+    }
+
+    enum DirectionToMove
+    {
+        up,
+        down,
+        left,
+        right
+    }
+}
