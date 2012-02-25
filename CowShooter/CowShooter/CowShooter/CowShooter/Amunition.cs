0f242baca6ea434436c22092b071c6c2efd1e96f
thk123 <thekileyenator@gmail.com>
2012-02-25 19:25:31 +0000

    Began ammunition class

---
+++ CowShooter/CowShooter/CowShooter/CowShooter/Amunition.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CowShooter
+{
+    class Amunition : ICollisionObject
+    {
+        Texture2D texture;
+        bool flying;
+        Vector2 velocity;
+
+        const float dampening = 0.1f;
+
+        public Rectangle collisionRectangle;
+
+        public Amunition(Catapult catapult, Vector2 startVelocity)
+        {
+
+        }
+
+        public Rectangle getCollisionRectangle()
+        {
+            return collisionRectangle;
+        }
+
+        public void NotifyOfCollision(ICollisionObject otherObject)
+        {
+            if (otherObject is Cow)
+            {
+                remove();
+            }
+            else if (true)
+            {
+
+            }
+        }
+
+        private void remove()
+        {
+
+        }
+
+        
+
+        public bool listenForGround()
+        {
+            return true;
+        }
+
+
+        public void NotifyGroundCollision()
+        {
+            velocity.Y = velocity.Y * -1 * dampening;
+            if (velocity.Y < 0.01f)
+            {
+                velocity.Y = 0;
+                flying = false;
+            }
+        }
+    }
+}
---
+++ CowShooter/CowShooter/CowShooter/CowShooter/ICollisionObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+
+namespace CowShooter
+{
+    interface ICollisionObject
+    {
+        public Rectangle getCollisionRectangle();
+
+        public bool listenForGround();
+        public void NotifyOfCollision(ICollisionObject otherObject);
+        public void NotifyGroundCollision();
+    }
+}
